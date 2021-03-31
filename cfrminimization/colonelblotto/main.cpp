86cc128006d72bb1baa25d3c360d84a33315999c
AbleDabble <wcary22@gmail.com>
2021-03-31 10:20:03 -0600

    quick fix

--- cfrminimization/colonelblotto/main.cpp
+++ cfrminimization/colonelblotto/main.cpp
@@ -4,10 +4,8 @@
 #include "game.h"
 using std::cout;
 using std::endl;
-using std::rand;
-
 int main() {
-  // cout << "hello world\n" << endl;
+  cout << "Sometimes my genius, it generates its own gravity" << endl;
   Game game = Game(3, 5);
   game.play(50000);
 }
